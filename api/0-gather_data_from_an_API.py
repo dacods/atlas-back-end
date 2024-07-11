b7135b1145767acc3c5c39a98fbcaa84b3f19479
dacods <dacods.21@gmail.com>
2024-07-11 16:02:18 -0500

    Made changes in 0-gather_data_from_an_API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -17,7 +17,7 @@ def todo_list_api(employee_id):
     todos_total_r = requests.get(todo_url, params=params)
     todos_total = todos_total_r.json()
     finished_task = [todo for todo in todos_total if todo['completed']]
-
+    
     print(
         f"Employee {employee_name} is done with tasks"
         f"({len(finished_task)}/{len(todos_total)}):"
